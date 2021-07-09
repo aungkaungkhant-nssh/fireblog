ca74914b23874933a32f914e386673002c79b5e3
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-07-09 13:48:17 +0630

    playing toggle button and show edit delete icon

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -5,14 +5,20 @@
                   <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" v-model="check">
                 <label class="form-check-label" for="flexSwitchCheckDefault">Toggle</label>
               </div>
+             
     </div>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity';
+import { onUpdated } from '@vue/runtime-core';
 export default {
-    setup(){
+    setup(props,context){
         let check=ref(false);
+        onUpdated(()=>{
+             context.emit("toggle",check.value)
+        })
+       
         return {check}
     }
 }
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,9 +2,21 @@
   <div class="container-fluid blog-card pt-5 pb-5">
     <div class="container">
         <div class="row">
-          <ToggleButton></ToggleButton>
+          <ToggleButton @toggle="toggle"></ToggleButton>
           <div class="col-xl-3 col-md-6 col-lg-4">
-              <div class="card mb-3">
+              <div class="card mb-3 blog-card">
+               <transition name="fade">
+                 <div v-if="showIcon" >
+                    <span class="material-icons trash">
+                    delete
+                    </span>
+                  <span class="material-icons edit">
+                    edit
+                  </span>
+                </div>
+               </transition>
+                
+                
                 <img src="../assets/media.png" alt="" style="height:200px">
                 <div class="card-body">
                   <h4>HTML Media Query Feature</h4>
@@ -20,7 +32,19 @@
               </div>
           </div>
          <div class="col-xl-3 col-md-6 col-lg-4">
-              <div class="card mb-3">
+              <div class="card mb-3 blog-card">
+                <transition name="fade">
+                      <div v-if="showIcon" >
+                          <span class="material-icons trash">
+                            delete
+                          </span>
+                          <span class="material-icons edit">
+                            edit
+                          </span>
+                    </div>
+                </transition>
+              
+               
                 <img src="../assets/boot.jpeg" alt="" style="height:200px">
                 <div class="card-body">
                   <h4>BootStrap Usage Of Feature</h4>
@@ -36,7 +60,18 @@
               </div>
           </div>
           <div class="col-xl-3 col-md-6 col-lg-4">
-              <div class="card mb-3">
+              <div class="card mb-3 blog-card">
+                  <transition name="fade">
+                       <div v-if="showIcon">
+                          <span class="material-icons trash">
+                            delete
+                          </span>
+                          <span class="material-icons edit">
+                            edit
+                          </span>
+                      </div>
+                  </transition>
+                 
                 <img src="../assets/laravel.png" alt="" style="height:200px">
                 <div class="card-body">
                   <h4>Laravel middleware Features</h4>
@@ -52,7 +87,18 @@
               </div>
           </div>
           <div class="col-xl-3 col-md-6 col-lg-4">
-              <div class="card mb-3">
+              <div class="card mb-3 blog-card">
+                  <transition name="fade">
+                         <div v-if="showIcon">
+                            <span class="material-icons trash">
+                              delete
+                            </span>
+                            <span class="material-icons edit">
+                              edit
+                            </span>
+                         </div>
+                  </transition>
+                 
                 <img src="../assets/ract.png" alt="" style="height:200px">
                 <div class="card-body">
                   <h4>React state usage dicuss</h4>
@@ -68,6 +114,7 @@
               </div>
           </div>
         </div>
+        
     </div>
   
   </div>
@@ -81,8 +128,11 @@ export default {
   components: { ToggleButton },
     name: 'Blog',
  setup(){
-   let check=ref(false);
-   return {check}
+   let showIcon=ref(null);
+   let toggle=(check)=>{
+     showIcon.value=check;
+   }
+   return{toggle,showIcon}
  }
 }
 </script>
@@ -92,9 +142,40 @@ export default {
   width: 100%;
   /* background-color: red; */
 }
+.card{
+  position: relative;
+}
 .toggle{
   color:#8395a7;
   font-size: 25px;
   cursor: pointer;
+}
+.trash{
+  position: absolute;
+  top:15px;
+  left: 15px;
+  color:#e84118;
+  background-color: #dcdde1;
+  border-radius: 50px;
+  padding: 5px 7px;
+}
+.edit{
+   position: absolute;
+  top:15px;
+  right: 15px;
+  color:blue;
+  background-color: #dcdde1;
+  border-radius: 50px;
+  padding: 5px 7px;
+}
+.blog-card span{
+  font-size: 35px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
