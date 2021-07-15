b1629ad23c1819e574975853fe2f2215b1c1a506
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-07-15 15:36:41 +0630

    file upload and playing disabled button

--- src/views/CreateBlog.vue
+++ src/views/CreateBlog.vue
@@ -7,10 +7,10 @@
                 </div>
                 <div class="col-md-2 mb-3">
                     <label for="actual-btn">Upload Photo</label>
-                    <input type="file"  id="actual-btn" hidden>
+                    <input type="file"  id="actual-btn" hidden @change="uploadImage" >
                 </div>
                 <div class="col-md-2 mt-3 mb-3">
-                    <button class="btn preview">Preview Photo</button>
+                    <button class="btn preview"  :disabled="buttonPlay">Preview Photo</button>
                 </div>
                 <editor
                 api-key="o19r3xqspo0dhqb8uz2ffu3sjflvfb9czudtseu72qdjfh1o"
@@ -31,8 +31,16 @@ export default {
     'editor': Editor
   },
   setup(){
-      let text=ref("akk")
-      return{text}
+      let text=ref("akk");
+      let file=ref(null);
+      let url=ref(null);
+      let buttonPlay=ref(true);
+      let uploadImage=(e)=>{
+          file.value=e.target.files[0];
+          url.value=URL.createObjectURL(file.value);
+          buttonPlay.value=false;
+      }
+      return{text,uploadImage,url,buttonPlay}
   }
 }
 </script>
